53b576fe194b2b726a41fe9c5054f584aa14f128
yoonucho <tomorrowcho@gmail.com>
2024-04-17 21:27:29 +0900

    Update dependencies and fix font import in layout.tsx

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,10 +1,9 @@
 // app/_app.tsx
 import type { AppProps } from "next/app";
-import { notoSansKR } from "./layout"; // 설정한 폰트를 가져옵니다.
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<div className={notoSansKR.className}>
+		<div>
 			<Component {...pageProps} />
 		</div>
 	);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,21 @@ import { Noto_Sans_KR } from "next/font/google";
 import "../styles/globals.css";
 import "../styles/custom.css";
 
-export const notoSansKR = Noto_Sans_KR({
+interface FontDetails {
+	style: {
+		fontFamily: string;
+		fontStyle: string;
+	};
+	className: string;
+	variable: string;
+}
+
+const notoSansKR: FontDetails = Noto_Sans_KR({
 	subsets: ["latin"], // 사용하고 싶은 폰트 서브셋을 배열 형태로 지정
 	weight: ["400", "500", "700"], // 사용하고 싶은 폰트 두께를 배열 형태로 지정
 	display: "swap", // 폰트 표시 옵션
 	variable: "--primary-font", // CSS 변수 이름
-});
+}) as FontDetails;
 
 export const metadata: Metadata = {
 	title: {
@@ -16,6 +25,9 @@ export const metadata: Metadata = {
 		default: "미용실 고객 관리 시스템 상세 페이지",
 	},
 	description: "미용실 고객 관리 시스템입니다.",
+	icons: {
+		icon: "/favicon.ico",
+	},
 };
 
 export default function RootLayout({
@@ -24,8 +36,9 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
+		// console.log(notoSansKR),
 		<html lang="ko">
-			<body className={notoSansKR.variable}>{children}</body>
+			<body className={notoSansKR.className}>{children}</body>
 		</html>
 	);
 }
