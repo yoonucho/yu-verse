f6be479bdb035756acad9b27d8ecc9b1dca7bd53
yoonucho <tomorrowcho@gmail.com>
2024-05-03 02:39:19 +0900

    Refactor ShowDetails component in page.tsx

--- src/app/result-info/show-details/page.tsx
+++ src/app/result-info/show-details/page.tsx
@@ -1,35 +1,14 @@
 "use client";
-import { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
 import Loading from "@/components/icons/LoadingIcon";
-import useQueryParams from "@/hooks/useQueryParams";
 import useFetchHolidays from "@/hooks/useFetchHolidays";
 import HolidayShowDetails from "@/components/result-info/HolidayShowDetails";
 import styles from "@/styles/result-info.module.css";
 
-interface Holiday {
-	id?: number;
-	title?: string;
-	start?: string;
-	dayOfWeek?: string;
-}
-
 export default function ShowDetails({}) {
-	const router = useRouter();
-	const [year, setYear] = useState("");
-	const queryParams = useQueryParams();
-	const startDate = queryParams.get("startDate");
-	const endDate = queryParams.get("endDate");
-
-	const { holidays, isLoading, error } = useFetchHolidays(startDate, endDate);
-
-	useEffect(() => {
-		const yearParam = new URLSearchParams(window.location.search).get("year");
-		setYear(yearParam);
-	}, [queryParams]);
+	const { holidays, isLoading, error } = useFetchHolidays();
 
 	if (isLoading) return <Loading />; // 로딩 중 처리
-	// if (error) return <p>{"문제가 발생하였습니다."}</p>; // 에러 처리
+	if (error) return <p>{"문제가 발생하였습니다."}</p>; // 에러 처리
 
 	return (
 		<div className={styles.container}>
