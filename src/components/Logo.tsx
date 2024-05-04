60355428fa88e86257a7a81cff0a2bf614ecd7ee
yoonucho <tomorrowcho@gmail.com>
2024-05-04 21:37:25 +0900

    Change logo image

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,7 +6,7 @@ import styles from "@/styles/logo.module.css";
 export default function Logo() {
 	return (
 		<div className={styles.iconContainer}>
-			<Image className={styles.icon} src="/assets/images/logo-icon.png" alt="logo icon" width={0} height={0} sizes="100vw" />
+			<Image className={styles.icon} src="/assets/images/bh_logo-w.png" alt="logo icon" width={0} height={0} sizes="100vw" />
 			<div className={`fa-2x ${styles.calendarIcon}`}>
 				<FontAwesomeIcon icon={faCalendar} />
 			</div>
