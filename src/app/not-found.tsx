dfcd1b9a5f5ea1b0502111aeb7338e27c0660d0d
yoonucho <tomorrowcho@gmail.com>
2024-05-04 18:39:49 +0900

    Build error ifx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 import styles from "../styles/not-found.module.css";
 import GoBack from "@/components/header/GoBack";
 
-export default function NotFound() {
+export default function NotFound({ isMenuOpen, handleClick }) {
 	return (
 		<main className={styles.container}>
-			<GoBack />
+			<GoBack isOpen={isMenuOpen} onClick={handleClick} />
 			<Image src="/assets/images/message-icon.svg" alt="not found" width={500} height={500} />
 			<h1>요청하신 페이지가 존재하지 않습니다.</h1>
 		</main>
