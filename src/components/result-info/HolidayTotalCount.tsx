e1e0167ae4f37f441dd638fbc27a5fe5120460f8
yoonucho <tomorrowcho@gmail.com>
2024-05-04 18:31:17 +0900

    refactor: Update metadata title and description for calendar search page

--- src/components/result-info/HolidayTotalCount.tsx
+++ src/components/result-info/HolidayTotalCount.tsx
@@ -4,7 +4,7 @@ export default function HolidayTotalCount({ count }) {
 	return (
 		<div className={styles.container}>
 			<p className={styles.text}>
-				공휴일 개수는 &nbsp;
+				공휴일은 &nbsp;
 				<code className={styles.code}>총 {count}일</code>
 				&nbsp; 입니다.
 			</p>
