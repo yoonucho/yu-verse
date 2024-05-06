0c51e29de31f86042a996fa35b58c4adaf198a9a
yoonucho <tomorrowcho@gmail.com>
2024-05-06 21:13:24 +0900

    feat: Add Open Graph meta tags for better social sharing

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,11 +1,22 @@
 // app/_app.tsx
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<div>
-			<Component {...pageProps} />
-		</div>
+		<>
+			<Head>
+				<title>YU캘린더</title>
+				<meta property="og:title" content="YU캘린더" />
+				<meta property="og:description" content="현재는 공휴일 검색만 가능합니다!" />
+				<meta property="og:image" content="/assets/images/yoonu-opengraph.png" />
+				<meta property="og:url" content="https://events-scheduler.vercel.app" />
+				<meta property="og:type" content="website" />
+			</Head>
+			<div>
+				<Component {...pageProps} />
+			</div>
+		</>
 	);
 }
 
