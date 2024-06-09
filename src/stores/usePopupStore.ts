72148a8991f64e2b211fed35db6b3765a71a231d
yoonucho <tomorrowcho@gmail.com>
2024-06-09 16:27:38 +0900

    Add popup position functionality to usePopupStore

--- src/stores/usePopupStore.ts
+++ src/stores/usePopupStore.ts
@@ -4,12 +4,16 @@ type PopupState = {
 	isPopupOpen: boolean;
 	openPopup: () => void;
 	closePopup: () => void;
+	popupPosition?: { x: number; y: number };
+	setPopupPosition?: (position: { x: number; y: number }) => void;
 };
 
 const usePopupStore = create<PopupState>(set => ({
 	isPopupOpen: false,
 	openPopup: () => set({ isPopupOpen: true }),
 	closePopup: () => set({ isPopupOpen: false }),
+	popupPosition: { x: 0, y: 0 },
+	setPopupPosition: position => set({ popupPosition: position }),
 }));
 
 export default usePopupStore;
