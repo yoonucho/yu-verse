f36919e40916f20e7fb329af6a753efd548d0c9f
yoonucho <tomorrowcho@gmail.com>
2024-10-13 16:35:20 +0900

    Feat: Split Supabase integration into SSR and client modules; implemented server actions for enhanced server-side features

--- packages/yu-calendar/src/lib/supabaseClient.ts
+++ packages/yu-calendar/src/lib/supabaseClient.ts
@@ -1,4 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
+"use client";
+
+import { createBrowserClient } from "@supabase/ssr";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
@@ -7,4 +9,4 @@ if (!supabaseUrl || !supabaseAnonKey) {
 	throw new Error("Missing env variables for Supabase");
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const createBrowserSupabaseClient = () => createBrowserClient(supabaseUrl!, supabaseAnonKey!);
