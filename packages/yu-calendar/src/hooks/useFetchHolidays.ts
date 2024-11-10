d7dc2d189f95adee0b9d4fea953790fe4e553ecc
yoonucho <tomorrowcho@gmail.com>
2024-11-10 17:26:54 +0900

    Feat: change loading state management from useFetchHolidays to useLoadingStore to local loading state localLoading, to setLocalLoading

--- packages/yu-calendar/src/hooks/useFetchHolidays.ts
+++ packages/yu-calendar/src/hooks/useFetchHolidays.ts
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { getYear, parseISO, isWithinInterval } from "date-fns";
 import getFetchHolidays from "@/app/api/holidayAPI";
 import useSetDateStore from "@/stores/useSetDateStore";
-import useLoadingStore from "@/stores/useLoadingStore";
 
 type Holiday = {
 	id?: string;
@@ -14,19 +13,25 @@ type Holiday = {
 const useFetchHolidays = () => {
 	const { startDate, endDate } = useSetDateStore();
 	const [holidays, setHolidays] = useState<Holiday[]>([]);
-	const { setIsLoading } = useLoadingStore();
+	const [localLoading, setLocalLoading] = useState(true); // 로컬 로딩 상태
 	const [error, setError] = useState(null);
 
 	useEffect(() => {
+		// startDate와 endDate가 모두 유효한 경우에만 데이터 요청을 시작
+		if (!startDate || !endDate) {
+			setLocalLoading(false); // 날짜가 없으면 로딩 종료
+			return;
+		}
+
 		const fetchHolidays = async () => {
-			setIsLoading(true);
+			setLocalLoading(true); // 로딩 상태 변경
 
 			try {
 				const fromYear = startDate ? getYear(parseISO(startDate.toISOString())) : null;
 				const toYear = endDate ? getYear(parseISO(endDate.toISOString())) : null;
 				if (!fromYear || !toYear) {
 					setError(new Error("Invalid dates"));
-					setIsLoading(false);
+					setLocalLoading(false);
 					return;
 				}
 				const promises: Promise<Holiday[]>[] = [];
@@ -46,15 +51,13 @@ const useFetchHolidays = () => {
 				console.error("Failed to fetch holidays:", error);
 				setError(error);
 				setHolidays([]);
+			} finally {
+				setLocalLoading(false); // 로딩 완료 시 false로 변경
 			}
-
-			setIsLoading(false);
 		};
 
-		if (startDate && endDate) {
-			fetchHolidays();
-		}
-	}, [startDate, endDate, setIsLoading]); // 변경될 때마다 useEffect 내부의 fetchHolidays 함수 실행
+		fetchHolidays();
+	}, [startDate, endDate, localLoading]); // 변경될 때마다 useEffect 내부의 fetchHolidays 함수 실행
 
 	return { holidays, error };
 };
