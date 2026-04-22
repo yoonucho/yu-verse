c67ef14518059a4341530244bb9773f4c44350ac
tomorrowcho <tomorrowcho@gmail.com>
2026-04-23 02:07:37 +0900

    chore: Supabase 교체로 인한 환경변수명 정리

--- packages/yu-calendar/src/app/middleware.ts
+++ packages/yu-calendar/src/app/middleware.ts
@@ -3,65 +3,73 @@ import { type NextRequest, NextResponse } from "next/server";
 import type { CookieMethodsServer } from "@supabase/ssr/dist/main/types";
 
 export const applyMiddlewareSupabaseClient = async (request: NextRequest) => {
-	// Create an unmodified response
-	let response = NextResponse.next({
-		request: {
-			headers: request.headers,
-		},
-	});
+  // Create an unmodified response
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
 
-	const supabase = createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
-		cookies: {
-			getAll() {
-				return request.cookies.getAll();
-			},
-			setAll(cookiesToSet) {
-				cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value));
-				response = NextResponse.next({
-					request,
-				});
-				cookiesToSet.forEach(({ name, value, options }) => response.cookies.set(name, value, options));
-			},
-			remove(name: string, options: CookieOptions) {
-				// If the cookie is removed, update the cookies for the request and response
-				request.cookies.set({
-					name,
-					value: "",
-					...options,
-				});
-				response = NextResponse.next({
-					request: {
-						headers: request.headers,
-					},
-				});
-				response.cookies.set({
-					name,
-					value: "",
-					...options,
-				});
-			},
-		} as CookieMethodsServer,
-	});
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            request.cookies.set(name, value),
+          );
+          response = NextResponse.next({
+            request,
+          });
+          cookiesToSet.forEach(({ name, value, options }) =>
+            response.cookies.set(name, value, options),
+          );
+        },
+        remove(name: string, options: CookieOptions) {
+          // If the cookie is removed, update the cookies for the request and response
+          request.cookies.set({
+            name,
+            value: "",
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
+          response.cookies.set({
+            name,
+            value: "",
+            ...options,
+          });
+        },
+      } as CookieMethodsServer,
+    },
+  );
 
-	// refreshing the auth token
-	await supabase.auth.getUser();
+  // refreshing the auth token
+  await supabase.auth.getUser();
 
-	return response;
+  return response;
 };
 
 export async function middleware(request) {
-	return await applyMiddlewareSupabaseClient(request);
+  return await applyMiddlewareSupabaseClient(request);
 }
 
 export const config = {
-	matcher: [
-		/*
-		 * Match all request paths except for the ones starting with:
-		 * - _next/static (static files)
-		 * - _next/image (image optimization files)
-		 * - favicon.ico (favicon file)
-		 * Feel free to modify this pattern to include more paths.
-		 */
-		"/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-	],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
 };
--- packages/yu-calendar/src/lib/supabaseServer.ts
+++ packages/yu-calendar/src/lib/supabaseServer.ts
@@ -7,34 +7,41 @@ import { Database } from "../../types_db";
 import type { CookieMethodsServer } from "@supabase/ssr/dist/main/types";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+const supabasePublishableKey =
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY || "";
 const supabaseServiceRole = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE || "";
 
 const createServerSupabaseClient = (
-	cookieStore: ReturnType<typeof cookies> = cookies(),
-	admin: boolean = false
+  cookieStore: ReturnType<typeof cookies> = cookies(),
+  admin: boolean = false,
 ): SupabaseClient<Database> => {
-	return createServerClient<Database>(supabaseUrl!, admin ? supabaseServiceRole! : supabaseAnonKey!, {
-		cookies: {
-			getAll() {
-				return cookieStore.getAll();
-			},
-			setAll(cookiesToSet) {
-				try {
-					cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options));
-				} catch (error) {
-					console.error("Failed to set cookies", error);
-				}
-			},
-			delete(name: string, options?: CookieOptions) {
-				try {
-					cookieStore.set({ name, value: "", ...options });
-				} catch (error) {
-					console.error("Failed to remove cookie", error);
-				}
-			},
-		} as CookieMethodsServer, // CookieMethodsServer로 명시적으로 타입 설정
-	}) as unknown as SupabaseClient<Database>;
+  return createServerClient<Database>(
+    supabaseUrl!,
+    admin ? supabaseServiceRole! : supabasePublishableKey!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options),
+            );
+          } catch (error) {
+            console.error("Failed to set cookies", error);
+          }
+        },
+        delete(name: string, options?: CookieOptions) {
+          try {
+            cookieStore.set({ name, value: "", ...options });
+          } catch (error) {
+            console.error("Failed to remove cookie", error);
+          }
+        },
+      } as CookieMethodsServer, // CookieMethodsServer로 명시적으로 타입 설정
+    },
+  ) as unknown as SupabaseClient<Database>;
 };
 
 export default createServerSupabaseClient;
